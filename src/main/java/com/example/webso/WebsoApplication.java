67aec3a0cbd60fbc94b91a663abb212ed52b2d88
mankasss <salakhetdinovd@mail.ru>
2022-05-18 21:54:39 +0300

    ddd

---
+++ src/main/java/com/example/webso/WebsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webso/WebsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
